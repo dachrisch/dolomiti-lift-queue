13ec8b7d8922a61eb32743c2abf0e4f8b46f8ba4
dachrisch <christian.daehn@gmail.com>
2023-11-27 18:50:01 +0100

    Rename key for consistency and fix queue value if None

--- tracker/state/__init__.py
+++ tracker/state/__init__.py
@@ -13,5 +13,9 @@ class LiftState(JsonDataclass):
     @classmethod
     def from_json(cls, lift_state: Dict[str, Any]):
         modified_lift_state_data = lift_state.copy()
+        # rename key for consistency
         modified_lift_state_data['metadata'] = modified_lift_state_data.pop('slopeOrLift')
+        # queue is sometimes None instead of 0, fix it
+        if not modified_lift_state_data["queue"]:
+            modified_lift_state_data["queue"] = 0
         return super().from_json(modified_lift_state_data)
